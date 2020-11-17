7f5d371928ba8f9940f93477a026cf1af37bad42
Marija Miletic <mileticmarijamalac@gmail.com>
2020-11-17 22:23:48 +0100

    Update Kalkulator.java

--- TerminIII/Kalkulator.java
+++ TerminIII/Kalkulator.java
@@ -4,12 +4,12 @@ import java.io.Console;
 
 public class Kalkulator {
     public static void main(String[] args) {
-        Console console=System.console();
-        System.out.println("Enter first operand: ");
-        String op1= console.readLine();
-        System.out.println("Enter second operand: ");
-        String op2= console.readLine();
-        System.out.println("Result is: ");
-        System.out.println(Integer.parseInt(op1)+Integer.parseInt(op2));
+        Console terminal=System.console();
+        terminal.writer().println("Enter first operand:");
+        int x= Integer.parseInt(terminal.readLine());
+        terminal.writer().println("Enter second operand:");
+        int y= Integer.parseInt(terminal.readLine());
+        terminal.writer().println("Result is:");
+        terminal.writer().println(x+y);
     }
 }
