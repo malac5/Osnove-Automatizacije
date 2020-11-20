ff94c845b1c9fd1f6feb358d8d6d77adc15920c9
Marija Miletic <mileticmarijamalac@gmail.com>
2020-11-20 17:33:51 +0100

    Update CTregisterPage.java

--- Termin XII/test/java/pom_classes/CTcreateAccount/CTregisterPage.java
+++ Termin XII/test/java/pom_classes/CTcreateAccount/CTregisterPage.java
@@ -3,13 +3,10 @@ package pom_classes.CTcreateAccount;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 public class CTregisterPage {
     WebDriver driver;
-    WebDriverWait wdWait;
-    String result;
     public CTregisterPage(WebDriver driver){
         this.driver=driver;
     }
